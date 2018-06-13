5343c405406118c7a247c9f94bc038e4aa815009
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-13 14:40:37 -0600

    Adds API/v1/maps and adds current map method to app controller

---
+++ app/controllers/api/v1/maps_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::MapsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    map = Map.new(map_params)
+    session[:map] = map.id
+    map.create_static
+    render js: "window.location = '#{new_order_path}'"
+  end
+
+  def show
+
+  end
+
+  private
+
+  def map_params
+    params.permit(:lat, :lng, :zoom)
+  end
+end
