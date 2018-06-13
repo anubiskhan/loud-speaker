4249f5d2ea2fa612720a751ddd0202fc87229b25
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-13 14:20:08 -0600

    Adds namespaced route for api v1 maps

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post '/maps', to: 'maps#create'
+    end
+  end
+  
   root 'welcome#index'
 
   get '/auth/google_oauth2', as: :google_oauth2_login
