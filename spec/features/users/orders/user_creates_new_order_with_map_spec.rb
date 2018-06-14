d0f8d450f43747d5a4970eaff4e36a600354c82a
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-14 14:35:01 -0600

    Adjusts test and adds new test for new order

--- spec/features/users/orders/user_creates_new_order_with_map_spec.rb
+++ spec/features/users/orders/user_creates_new_order_with_map_spec.rb
@@ -1,32 +1,24 @@
 require 'rails_helper'
 
-describe 'User', js: true do
-  describe 'creates a custom map' do
-    it 'creates current map without saving to DB' do
-      visit new_map_path
+describe 'User' do
+  describe 'on new order' do
+    it 'shows no map if they have not selected one' do
+      visit new_order_path
 
-      expect(Map.all.count).to eq(0)
-
-      click_on 'Use This Map'
-      sleep 2
-
-      expect(Map.all.count).to eq(0)
-      expect(current_path).to eq(new_order_path)
+      expect(page).to have_content("You've got to pick a map first!")
     end
-    xit 'targets the map properly' do
+    xit 'sees a static map of the map they want' do
       visit new_map_path
 
       fill_in 'address', with: 'spokane'
 
       click_on 'Encode'
-
+      sleep 2
       click_on 'Use This Map'
-
-      map = Map.last
-
+      sleep 2
       expected_url = "http://maps.googleapis.com/maps/api/staticmap?&center=39.7392,-104.99029999999999&zoom=14&size=750x450&key=AIzaSyCJwmkCQw56cvtU03ftjoN1SpzL1VmTov4&style=feature:administrative|element:all|saturation:-100&style=feature:administrative.province|element:all|visibility:off&style=feature:landscape|element:all|saturation:-100|lightness:65|visibility:on&style=feature:poi|element:all|visibility:off&style=feature:road|element:all|saturation:-100&style=feature:road.highway|element:all|visibility:simplified&style=feature:road.arterial|element:all|lightness:30&style=feature:road.local|element:all|lightness:40&style=feature:transit|element:all|saturation:-100|visibility:simplified&style=feature:water|element:geometry|hue:0xffff00|lightness:-25|saturation:-97&style=feature:water|element:labels|lightness:-25|saturation:-100"
 
-      expect(map['static_url']).to eq(expected_url)
+      expect(page.find('.map-image')['src']).to have_content(expected_url)
     end
   end
 end
