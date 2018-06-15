dbefdafbe7b86a089e76c1f9c044e643aec8cbf7
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-15 09:59:23 -0600

    Adds cart model spec

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Cart do
+  describe 'Instance methods' do
+    describe '#add_product' do
+      it 'adds a product to cart list' do
+        cart = Cart.new({})
+        cart.add_product(1)
+
+        expect(cart.list).to eq({'1' => 1})
+      end
+    end
+  end
+end
